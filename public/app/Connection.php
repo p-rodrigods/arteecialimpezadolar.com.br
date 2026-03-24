88c708386ba3844000c3e5930a225602ac9e4b54
p.rodrigods <117523178+p-rodrigods@users.noreply.github.com>
2026-03-24 10:03:44 -0300

    Modify database connection in Connection.php
    Updated database connection string to remove database name.

--- public/app/Connection.php
+++ public/app/Connection.php
@@ -6,15 +6,8 @@ class Connection
 {
     public static function getDb()
     {
-      /*try {
-            $conn = new \PDO("mysql:host=arteecialimpezadolar.com.br;dbname=u504967134_db_arteecia", "u504967134_db_arteecia", ">VoGF6;Vl1");
-            $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-            return $conn;
-        } catch (\PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-        }*/
         try {
-            $conn = new \PDO("mysql:host=localhost;dbname=db_arteecia", "root", "");
+            $conn = new \PDO("mysql:host=localhost;dbname=", "root", "");
             $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
             return $conn;
         } catch (\PDOException $e) {
